7184fc1ede33ca638cb9994db2490626c217e497
Emcikem <18968479985@163.com>
2022-04-26 15:07:26 +0800

    进度：在 XML 文件中定义 bean

--- src/test/java/com/lyq/minispring/test/ioc/XmlFileDefineBeanTest.java
+++ src/test/java/com/lyq/minispring/test/ioc/XmlFileDefineBeanTest.java
@@ -13,17 +13,17 @@ public class XmlFileDefineBeanTest {
 
 	@Test
 	public void testXmlFile() throws Exception {
-//		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
-//		XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
-//		beanDefinitionReader.loadBeanDefinitions("classpath:spring.xml");
-//
-//		Person person = (Person) beanFactory.getBean("person");
-//		System.out.println(person);
-//		assertThat(person.getName()).isEqualTo("derek");
-//		assertThat(person.getCar().getBrand()).isEqualTo("porsche");
-//
-//		Car car = (Car) beanFactory.getBean("car");
-//		System.out.println(car);
-//		assertThat(car.getBrand()).isEqualTo("porsche");
+		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+		XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
+		beanDefinitionReader.loadBeanDefinitions("classpath:spring.xml");
+
+		Person person = (Person) beanFactory.getBean("person");
+		System.out.println(person);
+		assertThat(person.getName()).isEqualTo("derek");
+		assertThat(person.getCar().getBrand()).isEqualTo("porsche");
+
+		Car car = (Car) beanFactory.getBean("car");
+		System.out.println(car);
+		assertThat(car.getBrand()).isEqualTo("porsche");
 	}
 }
